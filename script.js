ce3df6224763ee1f2f1f7ba09cc13500b6cc1888
BoasPraticasNoTransito <boaspraticasnotransitoetemaa@gmail.com>
2024-10-22 01:13:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// BPNT HOME PAGE JS
+
+// Header background color when scrolling
+
 window.addEventListener('scroll', function() {
     const header = document.getElementById('header');
     if (window.scrollY > 100) {
@@ -6,6 +10,36 @@ window.addEventListener('scroll', function() {
         header.style.backgroundColor = 'transparent'; // Original color
     }
 });
+
+// When search button is clicked
+
+document.getElementById('search-btn').addEventListener('click', function() {
+    var searchBar = document.getElementById('search-text');
+    searchBar.style.color = '#ff0000'; // Changes text color
+    searchBar.style.textShadow = '0.5px 0.5px 1px #000'; // Text shadow
+  
+    setTimeout(function() {
+        searchBar.style.color = ''; // Original color after 0.3s
+        searchBar.style.textShadow = ''; // Removes text shadow after 0.3s
+    }, 300);
+});
+
+// When user presses enter
+
+document.getElementById('search-bar').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        var searchButton = document.getElementById('search-icon');
+        searchButton.style.color = '#ff0000'; // Changes color of the icon
+        searchButton.style.textShadow = '0.5px 0.5px 1px #000'; // Icon's shadow
+      
+        setTimeout(function() {
+            searchButton.style.color = ''; // Original color after 0.3s
+            searchButton.style.textShadow = ''; // Removes the icon's shadow after 0.3s
+        }, 300);
+    }
+});  
+
+// Images of the home page
 
 document.addEventListener("DOMContentLoaded", function() {
     const images = [
@@ -24,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const section = document.querySelector('.section');
     section.style.backgroundImage = `url(${randomImage})`;
 
-    // Cria uma nova imagem para verificar quando a imagem de background estiver carregada
+    // Creates a new image to check when the background image has loaded
     const img = new Image();
     img.src = randomImage;
     img.onload = function() {
