227265a5fa95a71b8eee0a1cedc1465b136688d8
BoasPraticasNoTransito <boaspraticasnotransitoetemaa@gmail.com>
2024-11-26 12:19:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener('scroll', function() {
 document.getElementById('search-btn').addEventListener('click', function() {
     var searchBar = document.getElementById('search-text');
     searchBar.style.color = '#ff4800'; // Changes text color
-    searchBar.style.textShadow = '0.5px 0.5px 1px #000'; // Text shadow
+    searchBar.style.textShadow = '0.032rem 0.032rem 0.065rem #000'; // Text shadow
   
     setTimeout(function() {
         searchBar.style.color = ''; // Original color after 0.3s
@@ -30,87 +30,13 @@ document.getElementById('search-bar').addEventListener('keypress', function(even
     if (event.key === 'Enter') {
         var searchButton = document.getElementById('search-icon');
         searchButton.style.color = '#ff4800'; // Changes color of the icon
-        searchButton.style.textShadow = '0.5px 0.5px 1px #000'; // Icon's shadow
+        searchButton.style.textShadow = '0.032rem 0.032rem 0.065rem #000'; // Icon's shadow
       
         setTimeout(function() {
             searchButton.style.color = ''; // Original color after 0.3s
             searchButton.style.textShadow = ''; // Removes the icon's shadow after 0.3s
         }, 300);
     }
-});
-
-// When menu icon is clicked
-
-document.getElementById('icon2').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.style.transition = 'right 0.6s cubic-bezier(0.25, 0.8, 0.25, 1)';
-    menu.style.right = '0';
-});
-
-// When menu close icon is clicked
-
-document.getElementById('menu-close-icon').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.style.transition = 'right 0.6s cubic-bezier(0.25, 0.8, 0.25, 1)';
-    menu.style.right = '-100%';
-});
-
-// When user presses enter on the menu search bar
-
-document.getElementById('menu-search-text').addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        var menuSearchButton = document.getElementById('menu-search-btn');
-        menuSearchButton.style.backgroundColor = '#ff4800'; // Changes the button color
-
-        setTimeout(function() {
-            menuSearchButton.style.backgroundColor = ''; // Original color after 0.3s
-        }, 300);
-    }
-});
-
-// Images of the home page
-
-document.addEventListener("DOMContentLoaded", function() {
-    const images = [
-        'Jpg/Inicio/benjamin-punzalan-uMez7bFB2JQ-unsplash.jpg',
-        'Jpg/Inicio/clay-banks-KBqnxve73BE-unsplash.jpg',
-        'Jpg/Inicio/ed-259-xcrI6CPkkJs-unsplash.jpg',
-        'Jpg/Inicio/evans-dims-dyKl1WZDJ6M-unsplash.jpg',
-        'Jpg/Inicio/florian-wehde-8bjnP3yhNTg-unsplash.jpg',
-        'Jpg/Inicio/ilnur-kalimullin-CB0Qrf8ib4I-unsplash.jpg',
-        'Jpg/Inicio/jesse-bowser-c0I4ahyGIkA-unsplash.jpg',
-        'Jpg/Inicio/peggy-sue-zinn-pomI1itWE_0-unsplash.jpg',
-        'Jpg/Inicio/richard-haas-nGK4eApCxjM-unsplash.jpg'
-    ];
-
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    const section = document.querySelector('.section');
-    section.style.backgroundImage = `url(${randomImage})`;
-
-    // Creates a new image to check when the background image has loaded
-    const img = new Image();
-    img.src = randomImage;
-    img.onload = function() {
-        section.classList.add('loaded');
-    };
-});
-
-// Contents animation
-
-document.addEventListener('DOMContentLoaded', function () {
-    const fadeIns = document.querySelectorAll('.content1, .content2, .content3, .content4, .content5');
-
-    function checkFadeIn() {
-        fadeIns.forEach(section => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top < window.innerHeight - 100 && rect.bottom > 100) {
-                section.classList.add('fade-in');
-            }
-        });
-    }
-
-    window.addEventListener('scroll', checkFadeIn);
-    checkFadeIn();
 });
 
 // Search
@@ -215,6 +141,84 @@ function closeResults() {
     }, 300);
 }
 
+// When menu icon is clicked
+
+document.getElementById('icon2').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.style.transition = 'right 0.6s cubic-bezier(0.25, 0.8, 0.25, 1)';
+    menu.style.right = '0';
+});
+
+// When menu close icon is clicked
+
+document.getElementById('menu-close-icon').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.style.transition = 'right 0.6s cubic-bezier(0.25, 0.8, 0.25, 1)';
+    menu.style.right = '-100%';
+});
+
+// When user presses enter on the menu search bar
+
+document.getElementById('menu-search-text').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        var menuSearchButton = document.getElementById('menu-search-btn');
+        menuSearchButton.style.backgroundColor = '#ff4800'; // Changes the button color
+
+        setTimeout(function() {
+            menuSearchButton.style.backgroundColor = ''; // Original color after 0.3s
+        }, 300);
+    }
+});
+
+// Menu Search
+
+document.getElementById('menu-search-bar').onsubmit = async function(event) {
+    event.preventDefault();
+    const query = document.getElementById('menu-search-text').value;
+
+    const resultsContainer = document.getElementById('results');
+    const resultsContent = document.getElementById('results-content');
+    resultsContent.innerHTML = ''; // Cleans the previous content
+
+    const normalizedQuery = normalizeString(query);
+
+    const pages = [
+        { url: 'index.html', name: 'Início' },
+        { url: 'Sobre/sobre.html', name: 'Sobre' },
+        { url: 'Contato/contato.html', name: 'Contate-nos' },
+        { url: 'Blog/blog.html', name: 'Nosso Blog' }
+    ];
+
+    const results = [];
+
+    for (const { url, name } of pages) {
+        try {
+            const content = await fetchPageContent(url);
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(content, 'text/html');
+            const bodyText = doc.body.innerText;
+
+            const normalizedBodyText = normalizeString(bodyText);
+            if (normalizedBodyText.includes(normalizedQuery)) {
+                const startIndex = normalizedBodyText.indexOf(normalizedQuery);
+                const endIndex = Math.min(startIndex + normalizedQuery.length + 200, bodyText.length);
+                const excerpt = bodyText.slice(Math.max(0, startIndex - 20), endIndex);
+                
+                // Highlights the search in the content
+                
+                const highlightedExcerpt = excerpt.replace(new RegExp(`(${query})`, 'gi'), (match) => `<mark>${match}</mark>`);
+                
+                results.push({ page: name, url, content: highlightedExcerpt });
+            }
+        } catch (error) {
+            console.error('Error fetching page:', error);
+        }
+    }
+
+    displayResults(results, query);
+    document.getElementById('menu-search-text').value = ''; // Cleans the field
+};
+
 // Highlight on the page
 
 function highlightSearchTerms() {
@@ -261,50 +265,48 @@ function highlightSearchTerms() {
 
 document.addEventListener('DOMContentLoaded', highlightSearchTerms);
 
-// Menu Search
+// Images of the home page
 
-document.getElementById('menu-search-bar').onsubmit = async function(event) {
-    event.preventDefault();
-    const query = document.getElementById('menu-search-text').value;
-
-    const resultsContainer = document.getElementById('results');
-    const resultsContent = document.getElementById('results-content');
-    resultsContent.innerHTML = ''; // Cleans the previous content
-
-    const normalizedQuery = normalizeString(query);
-
-    const pages = [
-        { url: 'index.html', name: 'Início' },
-        { url: 'Sobre/sobre.html', name: 'Sobre' },
-        { url: 'Contato/contato.html', name: 'Contate-nos' },
-        { url: 'Blog/blog.html', name: 'Nosso Blog' }
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        'Jpg/Inicio/benjamin-punzalan-uMez7bFB2JQ-unsplash.jpg',
+        'Jpg/Inicio/clay-banks-KBqnxve73BE-unsplash.jpg',
+        'Jpg/Inicio/ed-259-xcrI6CPkkJs-unsplash.jpg',
+        'Jpg/Inicio/evans-dims-dyKl1WZDJ6M-unsplash.jpg',
+        'Jpg/Inicio/florian-wehde-8bjnP3yhNTg-unsplash.jpg',
+        'Jpg/Inicio/ilnur-kalimullin-CB0Qrf8ib4I-unsplash.jpg',
+        'Jpg/Inicio/jesse-bowser-c0I4ahyGIkA-unsplash.jpg',
+        'Jpg/Inicio/peggy-sue-zinn-pomI1itWE_0-unsplash.jpg',
+        'Jpg/Inicio/richard-haas-nGK4eApCxjM-unsplash.jpg'
     ];
 
-    const results = [];
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    const section = document.querySelector('.section');
+    section.style.backgroundImage = `url(${randomImage})`;
 
-    for (const { url, name } of pages) {
-        try {
-            const content = await fetchPageContent(url);
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(content, 'text/html');
-            const bodyText = doc.body.innerText;
+    // Creates a new image to check when the background image has loaded
+    
+    const img = new Image();
+    img.src = randomImage;
+    img.onload = function() {
+        section.classList.add('loaded');
+    };
+});
 
-            const normalizedBodyText = normalizeString(bodyText);
-            if (normalizedBodyText.includes(normalizedQuery)) {
-                const startIndex = normalizedBodyText.indexOf(normalizedQuery);
-                const endIndex = Math.min(startIndex + normalizedQuery.length + 200, bodyText.length);
-                const excerpt = bodyText.slice(Math.max(0, startIndex - 20), endIndex);
-                
-                // Highlights the search in the content
-                const highlightedExcerpt = excerpt.replace(new RegExp(`(${query})`, 'gi'), (match) => `<mark>${match}</mark>`);
-                
-                results.push({ page: name, url, content: highlightedExcerpt });
+// Contents animation
+
+document.addEventListener('DOMContentLoaded', function () {
+    const fadeIns = document.querySelectorAll('.content1, .content2, .content3, .content4, .content5');
+
+    function checkFadeIn() {
+        fadeIns.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top < window.innerHeight - 100 && rect.bottom > 100) {
+                section.classList.add('fade-in');
             }
-        } catch (error) {
-            console.error('Error fetching page:', error);
-        }
+        });
     }
 
-    displayResults(results, query);
-    document.getElementById('menu-search-text').value = ''; // Cleans the field
-};
+    window.addEventListener('scroll', checkFadeIn);
+    checkFadeIn();
+});
